d2ffed7259e53d9eff253c0e4deae36c8d47f5b9
HARSH VARDHAN PRASAD <114021101+harsh7274v@users.noreply.github.com>
2023-03-02 22:37:10 +0530

    Create simple code.c
    1st code .......late updation

---
+++ simple code.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a =10;
+  float a=10;
+  printf("%d",a);
+}
+
+#include<stdio.h>
+int main()
+{
+int i =-3;
+  int k = i%2;
+  printf("%d\n",k);
+  return 0;
+}
+
+
+
+#include <stdio.h>
+int main()
+{
+    int i = 3;
+    int l = i / -2;
+    int k = i % 2;
+
+    printf("%d %d\n",l,k);
+
+    return 0;
+}
+
+//________________________________________________________________
+
+#include <stdio.h>
+int main()
+{
+    int x = 5.3 % 2;
+
+    printf("%d\n",x);
+
+    return 0;
+}
+
+
